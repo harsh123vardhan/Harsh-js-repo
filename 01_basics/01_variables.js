dbf66510757076847979dab509e52c70525b5e36
harsh123vardhan <119279447+harsh123vardhan@users.noreply.github.com>
2025-02-24 10:57:55 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,7 @@
+const accountid = 4556
+let accountEmail="harshiitian123@gmail.com"
+var accountPassword="malviyan#12@"
 
+console.table([accountid,accountEmail,accountPassword])
+
+/* let and const use karo hamesha prefer not to used var
