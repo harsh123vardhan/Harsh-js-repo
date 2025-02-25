de7825f272739272e7477c606e0f909c9cb9ff63
harsh123vardhan <119279447+harsh123vardhan@users.noreply.github.com>
2025-02-25 08:19:30 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,4 +4,36 @@ var accountPassword="malviyan#12@"
 
 console.table([accountid,accountEmail,accountPassword])
 
-/* let and const use karo hamesha prefer not to used var
+/* let and const use karo hamesha prefer not to used var*/
+// typecast
+let number = "44"  // this is a string 
+console.log(typeof number);
+
+// now convert it into number by typecasting
+
+let valueNumber=Number(number);
+console.log(typeof number);
+
+//"44" --> 44
+//44abc --> NaN
+//true -->1
+
+let isLoggedin = "1"
+
+let booleanisLoggedin=Boolean(isLoggedin);
+console.log(booleanisLoggedin);                         // output is "true"
+
+
+// some more boolean conversions
+
+// 1 --> true
+// ""(empty) --> false
+//"harsh" --> true
+
+
+// Conversions into strings
+
+let someNumber=55
+let stringNumber=String(someNumber)
+
+console.log(typeof stringNumber)
